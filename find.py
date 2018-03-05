b219c3820dc23c637d9182c862ca79878208f967
tintuneal <tintupadikkal@gmail.com>
2018-03-05 14:42:03 +0530

    my first commit

---
+++ find.py
@@ -0,0 +1,8 @@
+message = 'Cristiano Ronaldo dos Santos Aveiro GOIH, ComM is a Portuguese professional footballer who plays as a forward for Spanish club Real Madrid and the Portugal national team'
+if message.find('is')==-1:
+	print('Not Present')
+else:
+	print('Present')
+	p=message.count('is')
+	print(p)
+
